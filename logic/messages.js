3a6f3f257f9f42f1fdace74eafef5dd54eb5009e
Antonio de la Vega <antoniodlavga@gmail.com>
2016-05-29 22:23:18 -0500

    New module that handles all messages

---
+++ logic/messages.js
@@ -0,0 +1,66 @@
+var request = require('request');
+var sensible = require('../security/sensible');
+
+var Message = {};
+
+Message.sendText = function (sender, text) {
+    var messageData = {
+        text: text
+    };
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {
+            access_token: sensible.token
+        },
+        method: 'POST',
+        json: {
+            recipient: {
+                id: sender
+            },
+            message: messageData
+        }
+    }, function (error, response, body) {
+        if (error) console.log('Error sending message: ', error);
+        else if (response.body.error) console.log('Error: ', response.body.error);
+    });
+};
+
+Message.sendStartGame = function (sender) {
+    var messageData = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                //image_url:
+                "elements": [{
+                    "title": "Mafia Chat Bot",
+                    "buttons": [{
+                        "type": "postback",
+                        "title": "Find game"
+                    }],
+                }]
+            }
+        }
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {
+            access_token: sensible.token
+        },
+        method: 'POST',
+        json: {
+            recipient: {
+                id: sender
+            },
+            message: messageData,
+        }
+    }, function (error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+};
+
+module.exports = Message;
