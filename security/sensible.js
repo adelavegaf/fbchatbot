425e6d0a65dc0c1c2acd1ec69b712adb6f1767d0
Antonio de la Vega <antoniodlavga@gmail.com>
2016-06-08 22:43:01 -0500

    Clear sensible fields.

--- security/sensible.js
+++ security/sensible.js
@@ -1,6 +1,6 @@
 var sensible = {
-    token: "EAAMAoeY3baQBABpJQ3s631YdZAEVF3nagkVeSIqsD8X1EaKQumdgsZBTCynLcToFkDpbZAzBhFaW7h5ixPucndqAB3Owdix2g1EHsNz3SokKL7HazOSZChEgsVBvXnmGZAMjlKDvnrwS0oQXMpdghRiEw3cAOrkGoom83BDW3ZAAZDZD",
-    verify_token: 'this_is_it'
+    token: "token",
+    verify_token: 'verify_token'
 };
 
 module.exports = sensible;
