01eb158419d606f041f5e32564f86d319a233c03
Antonio de la Vega <antoniodlavga@gmail.com>
2016-06-08 22:46:52 -0500

    Add token and verification

--- security/sensible.js
+++ security/sensible.js
@@ -1,6 +1,6 @@
 var sensible = {
-    token: "token",
-    verify_token: 'verify_token'
+    token: "EAAHDHPMHZCFwBAFw6bo8JZAvsMZCs3UcuhHn1KOdF0SZCVmwK85Sn9E4IuZAdWr6ZA5e3dEZCaRQBD16eAeQmxsk75wBd8DgZAs4NQcyHCHvi1LeWx1QLOyx3xsYNSxudBsoXuUdY4ZAu8v2LOZCyBovhu6EzeCZBWPhLp65fH7uFjcxAZDZD",
+    verify_token: 'mafiachatg4melaunch'
 };
 
 module.exports = sensible;
