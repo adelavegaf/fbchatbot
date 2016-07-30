841e0d52deafadb0b5d426538d43abcee1a1c508
Antonio de la Vega <antoniodlavga@gmail.com>
2016-07-30 18:26:12 -0500

    Update UI

--- scripts/controllers/gameplaycontroller.js
+++ scripts/controllers/gameplaycontroller.js
@@ -2,4 +2,16 @@
 
 angular.module('mafiaApp').controller('GameplayController', ['$scope', function ($scope) {
     $scope.title = 'Gameplay';
+
+    $scope.menuItems = [
+        {
+            name: 'General rules'
+        },
+        {
+            name: 'Phases'
+        },
+        {
+            name: 'Status bar'
+        }
+    ];
 }]);
