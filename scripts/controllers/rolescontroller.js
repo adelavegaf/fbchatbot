0fc9b32734e0f063934f0dd49a6d972898a640ed
Antonio de la Vega <antoniodlavga@gmail.com>
2016-07-31 19:09:15 -0500

    Add role and gameplay information.

--- scripts/controllers/rolescontroller.js
+++ scripts/controllers/rolescontroller.js
@@ -3,5 +3,41 @@
 angular.module('mafiaApp').controller('RolesController', ['$scope', function ($scope) {
     $scope.title = 'Roles';
 
-    $scope.tabItems = ['Doctor', 'Vigilante', 'Mafia Boss', 'Mafioso', 'Fixer', 'Barman', 'Investigator'];
+    $scope.tabItems = ['Doctor', 'Vigilante', 'Mafia Boss', 'Mafioso', 'Fixer', 'Barman', 'Detective'];
+
+    $scope.selectedItem = '';
+
+    $scope.information = {};
+
+    $scope.selectItem = function (item) {
+        $scope.selectedItem = item;
+    };
+
+    $scope.information['Doctor'] = [
+
+    ];
+
+    $scope.information['Vigilante'] = [
+
+    ];
+
+    $scope.information['Mafia Boss'] = [
+
+    ];
+
+    $scope.information['Mafioso'] = [
+
+    ];
+
+    $scope.information['Fixer'] = [
+
+    ];
+
+    $scope.information['Barman'] = [
+
+    ];
+
+    $scope.information['Detective'] = [
+
+    ];
 }]);
